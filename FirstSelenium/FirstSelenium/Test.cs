b75d8c27792e243054d65c4bea8954e435b9fb93
sdemewoz <selamawit.demewoz@albelli.com>
2018-11-13 21:39:47 +0100

    First Selenium test example

---
+++ FirstSelenium/FirstSelenium/Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.Html5;
+using OpenQA.Selenium.Interactions;
+
+namespace FirstSelenium
+{
+    class Test
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver(@"C:\Users\sdemewoz\Downloads");
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Window.Maximize();
+            //driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(2);
+        }
+
+        [Test]
+        public void RunTests()
+        {
+            OpenGoogle();
+            SearchAlbelli();
+        }
+        
+        public void OpenGoogle()
+        {
+            driver.Url = "http://www.google.com";
+        }
+
+        public void SearchAlbelli()
+        {
+            string searchText = "albelli";
+            driver.FindElement(By.ClassName("gsfi")).SendKeys(searchText);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            //driver.FindElement(By.Name("btnK")).Click(); //error b/c it is not clickable. dont know why
+            //press enter to search
+            Actions action = new Actions(driver);
+            action.SendKeys(OpenQA.Selenium.Keys.Enter).Build().Perform();
+
+            // checks if the result contains the searchText
+            IWebElement result = driver.FindElement(By.Id("search"));
+            Assert.IsTrue(result.Text.Contains("albelli"), "Albelli is not found");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
