fa6b5f5e188c3fdd16f5fb99cf0cf389302adf09
sdemewoz <selamawit.demewoz@albelli.com>
2018-12-03 17:58:21 +0100

    New test added

--- FirstSelenium/FirstSelenium/Test.cs
+++ FirstSelenium/FirstSelenium/Test.cs
@@ -20,7 +20,7 @@ namespace FirstSelenium
         [SetUp]
         public void StartBrowser()
         {
-            driver = new ChromeDriver(@"C:\Users\sdemewoz\Downloads");
+            driver = new ChromeDriver(@"C:\Users\sdemewoz\Downloads"); //chromedriver.exe should be in git and point to that folder
             driver.Manage().Cookies.DeleteAllCookies();
             driver.Manage().Window.Maximize();
             //driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(2);
@@ -41,16 +41,17 @@ namespace FirstSelenium
         public void SearchAlbelli()
         {
             string searchText = "albelli";
-            driver.FindElement(By.ClassName("gsfi")).SendKeys(searchText);
+            driver.FindElement(By.Name("q")).SendKeys(searchText);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             //driver.FindElement(By.Name("btnK")).Click(); //error b/c it is not clickable. dont know why
+
             //press enter to search
             Actions action = new Actions(driver);
             action.SendKeys(OpenQA.Selenium.Keys.Enter).Build().Perform();
 
             // checks if the result contains the searchText
             IWebElement result = driver.FindElement(By.Id("search"));
-            Assert.IsTrue(result.Text.Contains("albelli"), "Albelli is not found");
+            Assert.IsTrue(result.Text.Contains(searchText), "Albelli is not found");
         }
 
         [TearDown]
